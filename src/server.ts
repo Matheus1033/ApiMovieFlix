127962b3ee54f2f0c3e7dd488c8f0d5eddf3046a
Aquino0 <cristopheraquino20@gmail.com>
2024-11-26 20:13:45 -0300

    configurando mensagem de falha no cadastro de um filme

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,37 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
-    const movies = await prisma.movie.findMany();
+    const movies = await prisma.movie.findMany({
+        orderBy: { title: "asc" },
+        include: { genres: true, languages: true },
+    });
     res.json(movies);
 });
 
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                // cuidado aqui, o mes começa em 0 e vai até 11
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (error) {
+        res.status(500).send({ message: "Falha ao tentar cadastrar o filme" });
+    }
+    res.status(201).send();
+});
+
 app.listen(port, () => {
-    console.log(`Servidor em execução em http://localhost:${port}`);
+    console.log(`Servidor em execução na porta ${port}`);
 });
