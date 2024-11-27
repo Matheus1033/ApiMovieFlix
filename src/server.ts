e7d93eb2fc8ec6fe273e47a4b0f4dc35d5f20f47
Aquino0 <cristopheraquino20@gmail.com>
2024-11-27 19:33:12 -0300

    Adicionado filtro de filmes

--- src/server.ts
+++ src/server.ts
@@ -91,6 +91,28 @@ app.delete("/movies/:id", async (req, res) => {
     res.status(200).send();
 });
 
+app.get("/movies/:genderName", async (req, res) => {
+    try {
+        const moviesFilteredByGenderName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true,
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genderName,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+
+        res.status(200).send(moviesFilteredByGenderName);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar um filme" });
+    }
+});
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
